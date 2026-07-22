83b35dfef11becbeb64f8a1dbc748eef8799132a
Naman Rajput <namanrajput101112@gmail.com>
2026-07-22 09:01:42 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import "./config/db.js";
-
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Backend Running 🚀");
 });
